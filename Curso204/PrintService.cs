683317ba84d419891530b9e9d945d50263639561
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2021-08-21 17:10:12 -0300

    Commit inicial, criamos a classe PrintService e seus dois métodos, iniciamos o programa principal.

---
+++ Curso204/PrintService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso204
+{
+    class PrintService
+    {
+        private int[] _values = new int[10];
+        private int _count = 0;
+
+        public void AddValue(int value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("Print service is full.");
+            }
+            _values[_count++] = value;
+        }
+
+        public int First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("Print service empty.");
+            }
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < (_count-1); i++)
+            {
+                Console.Write(_values[i]+ ", ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(_values[_count-1]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ Curso204/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Curso204;
+
+PrintService ps = new PrintService();
+Console.WriteLine("How many values: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    ps.AddValue(x);
+}
+
+ps.Print();
+
+Console.WriteLine("First: " + ps.First());
